8961a652d820711bd8c4d50c8204432550a95307
jorgepertuz23 <jorgepertuz23@gmail.com>
2023-01-23 02:01:39 -0500

    commit 23 01 2023 hora 2:00 am, el prepcourse ha sido completado exitosamente hasta el modulo #6

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -51,44 +51,78 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
-   objeto[metodo];
+   objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
    // Tu código:
+   var auxi=objetoMisterioso.numeroMisterioso*5;
+   return auxi;
+
+
 }
 
 function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
+   delete objeto[propiedad];
+   return objeto;
 }
 
 function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar True. Caso contrario, False.
    // Tu código:
+ 
+   var validador=objetoUsuario.hasOwnProperty('email');   
+   if(validador==true){
+      if(objetoUsuario.email==null){
+          validador=false;
+          return validador
+
+      }      
+   }
+   return validador;
 }
 
 function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar True. Caso contrario, False.
    // Tu código:
+   var validador=objeto.hasOwnProperty(propiedad);
+   return validador;
 }
 
 function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar True. Caso contrario, False.
    // Tu código:
-}
+
+   var c1=objetoUsuario.password;
+   if(c1==password){
+      return true;
+   }else{
+      return false;
+   }
+
+   }
+
+
+
+
+
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
    // Reemplaza la contrseña guardada en la propiedad "password" del "objetoUsuario".
    // La nueva contraseña la recibes por parámetro.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario.password=nuevaPassword;
+   return objetoUsuario;
+
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
@@ -96,6 +130,8 @@ function agregarAmigo(objetoUsuario, nuevoAmigo) {
    // Debes agregar el "nuevoAmigo" al final de este arreglo.
    // Retornar el objeto.
    // Tu código:
+   (objetoUsuario.amigos).push(nuevoAmigo);
+   return objetoUsuario;
 }
 
 function pasarUsuarioAPremium(objetoMuchosUsuarios) {
@@ -104,6 +140,14 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Define esta propiedad de todos los usuarios como True.
    // Retornar el arreglo.
    // Tu código:
+   objetoMuchosUsuarios.forEach(item => {
+      item.esPremium=true;
+
+   
+  });
+
+ return objetoMuchosUsuarios;
+
 }
 
 function sumarLikesDeUsuario(objetoUsuario) {
@@ -112,6 +156,18 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
    // Tu código:
+   let suma=0;
+   var array=objetoUsuario.posts;
+   
+   array.forEach(element => {
+      suma+=element.likes;
+
+      
+   });
+
+return suma;
+
+
 }
 
 function agregarMetodoCalculoDescuento(objetoProducto) {
@@ -125,6 +181,13 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
+   objetoProducto.calcularPrecioDescuento =function(porcentajeDeDescuento,precio){
+      var dcto=(this.precio)*(this.porcentajeDeDescuento)
+      var final=this.precio-dcto
+      return final};
+
+      return objetoProducto;
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
